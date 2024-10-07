17350bd063853afa476f9687ab29cfce3670c2a4
kbarnaverebely <kovacs.barna@verebelyszki.hu>
2024-10-07 11:51:19 +0200

    Add files via upload

---
+++ Kovacs_Barna_triatlon/Eredmenyek.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Kovacs_Barna_triatlon
+{
+    internal class Eredmenyek
+    {
+        public string Nev { get; set; }
+        public int Szulev { get; set; }
+        public string Rajtszam { get; set; }
+        public string Nem { get; set; }
+        public string Kategoria { get; set; }
+        public TimeOnly Uszasido { get; set; }
+        public TimeOnly Elsoido { get; set; }
+        public TimeOnly Kerekparido { get; set; }
+        public TimeOnly Masodikido { get; set; }
+        public TimeOnly Futasido { get; set; }
+    }
+   /* public override string ToString()
+    {
+        return $"{Nev} ({Szulev} Rajtszám: {Rajtszam}. {Kategoria})";
+    }*/
+}
---
+++ Kovacs_Barna_triatlon/Program.cs
@@ -0,0 +1,84 @@
+﻿using Kovacs_Barna_triatlon;
+
+List<Eredmenyek> eredmeny = [
+    new Eredmenyek() //1
+     {
+         Nev = "Ábrahám Richárd",
+         Szulev = 1998,
+         Rajtszam = "159",
+         Nem = "f",
+         Kategoria = "16-17",
+         Uszasido = new TimeOnly(00,24,03),
+         Elsoido = new TimeOnly(00,01,54),
+         Kerekparido = new TimeOnly(00,45,15),
+         Masodikido = new TimeOnly(00,01,15),
+         Futasido = new TimeOnly(00,25,15)
+
+     },
+    new Eredmenyek() //2
+     {
+         Nev = "Ábrahám Richárd",
+         Szulev = 1998,
+         Rajtszam = "159",
+         Nem = "f",
+         Kategoria = "16-17",
+         Uszasido = new TimeOnly(00,24,03),
+         Elsoido = new TimeOnly(00,01,54),
+         Kerekparido = new TimeOnly(00,45,15),
+         Masodikido = new TimeOnly(00,01,15),
+         Futasido = new TimeOnly(00,25,15)
+
+     }
+    new Eredmenyek() //3
+     {
+         Nev = "Ábrahám Richárd",
+         Szulev = 1998,
+         Rajtszam = "159",
+         Nem = "f",
+         Kategoria = "16-17",
+         Uszasido = new TimeOnly(00,24,03),
+         Elsoido = new TimeOnly(00,01,54),
+         Kerekparido = new TimeOnly(00,45,15),
+         Masodikido = new TimeOnly(00,01,15),
+         Futasido = new TimeOnly(00,25,15)
+
+     },
+     new Eredmenyek() //4
+     {
+         Nev = "Ábrahám Richárd",
+         Szulev = 1998,
+         Rajtszam = "159",
+         Nem = "f",
+         Kategoria = "elit junior",
+         Uszasido = new TimeOnly(00,24,03),
+         Elsoido = new TimeOnly(00,01,54),
+         Kerekparido = new TimeOnly(00,45,15),
+         Masodikido = new TimeOnly(00,01,15),
+         Futasido = new TimeOnly(00,25,15)
+
+     },
+     new Eredmenyek() //5
+     {
+         Nev = "Ábrahám Richárd",
+         Szulev = 1998,
+         Rajtszam = "159",
+         Nem = "f",
+         Kategoria = "elit junior",
+         Uszasido = new TimeOnly(00,24,03),
+         Elsoido = new TimeOnly(00,01,54),
+         Kerekparido = new TimeOnly(00,45,15),
+         Masodikido = new TimeOnly(00,01,15),
+         Futasido = new TimeOnly(00,25,15)
+
+     },
+    ];
+
+Console.WriteLine($"A versenyt {eredmeny.Count} fő fejezte be");
+int ej = 0;
+for ( int i = 0; i < eredmeny.Count; i++)
+{
+    if (eredmeny.Kategoria = "elit junior")
+    {
+        ej++;
+    }
+}
